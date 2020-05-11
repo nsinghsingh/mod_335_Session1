ced3c88486aa3dbd5bd254d3665d7401fc659286
nsinghsingh <nihal.singh@csbe.ch>
2020-05-11 12:20:27 +0200

    added class

---
+++ src/BankAccount.java
@@ -0,0 +1,13 @@
+public class BankAccount {
+
+    public String firstName;
+    public String lastName;
+
+    private BankAccount(){
+
+    }
+    public BankAccount(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
